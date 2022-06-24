28542031c430ca54862b870bd6d5e244a0a50098
Eric Moran <emoran2418@fb.com>
2022-06-23 22:18:06 -0700

    Created segue between home timeline and details

---
+++ twitter/View Controllers/DetailsViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailsViewController.h
+//  twitter
+//
+//  Created by Eric Moran on 6/23/22.
+//  Copyright © 2022 Emerson Malca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ twitter/View Controllers/DetailsViewController.m
@@ -0,0 +1,32 @@
+//
+//  DetailsViewController.m
+//  twitter
+//
+//  Created by Eric Moran on 6/23/22.
+//  Copyright © 2022 Emerson Malca. All rights reserved.
+//
+
+#import "DetailsViewController.h"
+
+@interface DetailsViewController ()
+
+@end
+
+@implementation DetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
